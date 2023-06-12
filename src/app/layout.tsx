8d7c8ab71ef0f10eb17c17d347f375b30fd851b8
Luke Berry <lukeberrypi@gmail.com>
2023-06-12 07:05:13 -0300

    developHomePage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,17 +21,73 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`bg-zinc-950 text-zinc-100 ${dmSans.className}`}>
-        <nav className="text-white fixed top-0 text-lg bg-zinc-950 w-full px-6 py-4 border-b border-zinc-800">
-          <ul className="flex w-full items-center justify-between">
+        <nav className="text-white items-center justify-between flex fixed top-0 text-lg bg-zinc-950 w-full px-8 py-5 border-b border-zinc-900">
+          <Link
+            href="/"
+            className="transition-all active:translate-y-1 flex gap-4 items-center"
+          >
+            <h1 className="text-2xl font-bold">css2wind </h1>
+          </Link>
+          <ul className="flex items-center justify-between gap-10">
+            <li>
+              <Link
+                className="hover:border-zinc-200 border border-zinc-900 transition-all py-2 px-5"
+                href="#"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="hover:border-zinc-200 border border-zinc-900 transition-all py-2 px-5"
+                href="#"
+              >
+                About
+              </Link>
+            </li>
             <li className="transition-all active:translate-y-1">
-              <Link href="/" className="flex gap-4 items-center">
-                <h1 className="text-2xl font-bold">css2wind</h1>
+              <Link
+                className="border hover:bg-berryBlue border-berryBlue hover:text-black text-berryBlue py-2 px-4"
+                href="#"
+              >
+                Sign Up
               </Link>
             </li>
           </ul>
         </nav>
         {children}
-        <footer className="fixed border-t border-zinc-800 flex items-center bg-zinc-950 w-full bottom-0 py-5 justify-center">
+        <footer className="border-t text-lg border-zinc-900 flex items-center bg-zinc-950 w-full bottom-0 py-5 px-8 justify-between">
+          <ul className="flex items-center gap-8">
+            <li>
+              <Link
+                href="https://github.com/LukeberryPi/css2wind"
+                target="_blank"
+                className="underline underline-offset-2"
+              >
+                GitHub
+              </Link>
+            </li>
+            ▪
+            <li>
+              <Link
+                href="https://github.com/LukeberryPi/css2wind"
+                target="_blank"
+                className="underline underline-offset-2"
+              >
+                Twitter
+              </Link>
+            </li>
+            ▪
+            <li>
+              <Link
+                href="https://github.com/LukeberryPi/css2wind"
+                target="_blank"
+                className="underline underline-offset-2"
+              >
+                Instagram
+              </Link>
+            </li>
+          </ul>
           <p>© 2023 LukeberryPi</p>
         </footer>
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,121 @@
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+
 export default function Home() {
-  return <div>hello</div>;
+  return (
+    <main className="flex-col pt-20 flex h-full w-full text-center justify-center">
+      <section
+        id="hero-banner"
+        className="border-b container mx-auto border-zinc-900 flex flex-col gap-10 items-center justify-center py-36"
+      >
+        <h1 className="text-6xl font-bold text-zinc-100">
+          Learn TailwindCSS
+          <span className="text-berryBlue"> by playing</span>
+        </h1>
+        <p className="text-2xl text-zinc-400">
+          Dive into the most ergonomic styling framework and{" "}
+          <strong className="text-zinc-200">boost your development </strong>
+          speed through gamified learning.
+        </p>
+        <div className="flex gap-16 items-center">
+          <Link
+            href="/play"
+            className="flex border active:translate-y-1 group items-center gap-2 transition-all hover:bg-berryBlue border-berryBlue py-4 pr-8 pl-5"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path
+                className="group-hover:fill-black fill-berryBlue"
+                d="M10 8.64L15.27 12L10 15.36V8.64M8 5v14l11-7L8 5z"
+              />
+            </svg>
+            <p className="text-xl group-hover:text-black text-berryBlue">
+              Play Now
+            </p>
+          </Link>
+          <Link
+            className="hover:border-zinc-200 border border-zinc-900 py-4 px-8 text-xl"
+            href="#benefits"
+          >
+            Learn More
+          </Link>
+        </div>
+      </section>
+      <section
+        id="benefits"
+        className="border-b max-w-5xl mx-auto border-zinc-900 flex flex-col gap-36 items-center justify-center py-32"
+      >
+        <div className="flex items-center gap-32 w-full">
+          <div className="flex text-left flex-col gap-6 w-1/2">
+            <h3 className="text-5xl text-zinc-200">Immediate Feedback</h3>
+            <p className="text-xl text-zinc-400">
+              Gamified learning offers instant feedback on performance, allowing
+              learners to understand their strengths and areas for improvement,
+              enabling them to adjust their strategies accordingly.
+            </p>
+          </div>
+          <Image src="/home.png" alt="White square" width={450} height={225} />
+        </div>
+        <div className="flex items-center gap-32 w-full">
+          <Image src="/home.png" alt="White square" width={450} height={225} />
+          <div className="flex text-left flex-col gap-6 w-1/2">
+            <h3 className="text-5xl text-zinc-200">Active Learning</h3>
+            <p className="text-xl text-zinc-400">
+              Games encourage active participation, requiring players to make
+              decisions, solve problems, and apply knowledge in real-time,
+              leading to deeper understanding and retention of concepts.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section
+        id="faq"
+        className="flex flex-col gap-8 py-32 mx-auto min-w-[1024px]"
+      >
+        <h2 className="text-5xl font-medium self-center text-zinc-100">
+          Frequently Asked Questions
+        </h2>
+        <details
+          open
+          className="max-w-5xl flex text-left border hover:border-zinc-100 transition-all border-zinc-800 w-full py-4 px-6 cursor-pointer"
+        >
+          <summary className="w-full">How does the details-tag work?</summary>
+          <p className="pt-4 text-zinc-400">
+            The details tag works as an interactive widget that hides / shows
+            content.
+          </p>
+        </details>
+        <details className="cursor-pointer max-w-5xl flex text-left border hover:border-zinc-100 transition-all border-zinc-800 w-full py-4 px-6">
+          <summary className="w-full">How does the summary-tag work?</summary>
+          <p className="pt-4 text-zinc-400">
+            The summary tag works as the heading of the details tag. The summary
+            will always be shown and can be clicked to show or hide the other
+            content of the details-tag
+          </p>
+        </details>
+        <details className="cursor-pointer max-w-5xl flex text-left border hover:border-zinc-100 transition-all border-zinc-800 w-full py-4 px-6">
+          <summary className="w-full">
+            What content can I put in the details-tag?
+          </summary>
+          <p className="mt text-zinc-400-4">
+            You can put any content you want inside of the details-tag, though
+            the summary-tag should be the first child.
+          </p>
+        </details>
+        <details className="cursor-pointer max-w-5xl flex text-left border hover:border-zinc-100 transition-all border-zinc-800 w-full py-4 px-6">
+          <summary className="w-full">
+            Can I have a details-tag open by default?
+          </summary>
+          <p className="mt text-zinc-400-4">
+            Yes you can, simply add the open attribute to the details-tag
+          </p>
+        </details>
+      </section>
+    </main>
+  );
 }
