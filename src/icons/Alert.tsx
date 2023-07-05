ea7106c15402c2c72e09ecc301e18781fe4259cb
Luke Berry <lukeberrypi@gmail.com>
2023-07-04 22:04:31 -0300

    change alert icon

--- src/icons/Alert.tsx
+++ src/icons/Alert.tsx
@@ -9,7 +9,7 @@ export function Alert({ className, size = 24 }: IconProps) {
       viewBox="0 0 24 24"
       className={className}
     >
-      <path d="M3 3h16v2H5v14h14v2H3V3zm18 0h-2v18h2V3zM11 15h2v2h-2v-2zm2-8h-2v6h2V7z" />
+      <path d="M13 1h-2v2H9v2H7v2H5v2H3v2H1v2h2v2h2v2h2v2h2v2h2v2h2v-2h2v-2h2v-2h2v-2h2v-2h2v-2h-2V9h-2V7h-2V5h-2V3h-2V1zm0 2v2h2v2h2v2h2v2h2v2h-2v2h-2v2h-2v2h-2v2h-2v-2H9v-2H7v-2H5v-2H3v-2h2V9h2V7h2V5h2V3h2zm0 4h-2v6h2V7zm0 8h-2v2h2v-2z" />
     </svg>
   );
 }
