8c6622266373d3b9713ed1e4c764ad5db1f75743
Luke Berry <lukeberrypi@gmail.com>
2023-07-15 16:02:27 -0300

    update api route

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
-import { propertyDictionary } from "@/leozada";
 import { api } from "@/experimental-json";
 
 export async function GET() {
-  return NextResponse.json({ ...api, ...propertyDictionary });
+  return NextResponse.json(api);
 }
