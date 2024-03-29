931f3bb19dab17748a47fcbeac20a71eebc95588
Luke Berry <lukeberrypi@gmail.com>
2024-03-28 23:07:55 -0300

    add resources page and remove unused deps

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -1,57 +1,21 @@
 import { Open } from "@/icons";
 
-export const metadata = {
-  title: "css2wind | Frequently Asked Questions",
-  description: "Learn more about Tailwind CSS.",
-  applicationName: "css2wind",
-  referrer: "origin-when-cross-origin",
-  keywords: ["Tailwind", "CSS", "Bootstrap", "Style", "HTML"],
-  authors: { name: "Luke Berry" },
-  creator: "Luke Berry",
-  publisher: "Luke Berry",
-  openGraph: {
-    type: "website",
-    images: "https://www.css2wind.com/og-faq.png",
-    siteName: "css2wind",
-    title: "css2wind | Frequently Asked Questions",
-    description: "Learn more about Tailwind CSS.",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "css2wind, a minigame for learning Tailwind CSS",
-    description:
-      "What is Tailwind? Isn't that just inline styles? Isn't that just Bootstrap? Doesn't the HTMl get ugly?",
-    images: { url: "https://www.css2wind.com/og-faq.png" },
-  },
-  verification: {
-    google: "pWGXfs8mFFqr-ZqzvuVT82rztzmghIcc6gwy_qklleo",
-  },
-};
-
-export default function Faq() {
+export default function Resources() {
   return (
     <main className="container mx-auto my-36 flex w-full flex-col gap-20 px-6">
       <h2 className="text-center text-5xl text-zinc-200 md:text-6xl">
-        Frequently Asked Questions
+        Resources
       </h2>
       <ul className="mx-auto flex max-w-[800px] flex-col gap-20">
         <li id="what" className="flex flex-col gap-5 px-2 md:px-0 md:text-left">
           <h3 className="text-2xl text-zinc-200 sm:text-4xl">
-            What is{" "}
-            <span className="bg-sky-300 px-2 font-black text-zinc-900">
-              Tailwind
-            </span>
-            <span className="bg-sky-300 px-2 font-black text-zinc-900">
-              CSS?
-            </span>
+            Tailwind Intellisense
           </h3>
           <p className="text-lg text-zinc-400 sm:text-xl">
-            Tailwind CSS is a different way of writing CSS. It provides small
-            pieces of CSS, called utilities, which represent a single line of
-            CSS. It encourages colocation (keeping markdown and styles in the
-            same place) for easier maintenance. This means you don&apos;t need
-            to worry about naming your classes or styles affecting unwanted
-            elements.
+            This VS Code extension allows auto-completion while you write
+            Tailwind classes! This boosts productivity and can even teach you
+            new utilities! It is also useful for finding out what CSS a utility
+            hovering
           </p>
           <div className="flex flex-col gap-2 text-left text-zinc-200 sm:flex-row sm:text-lg md:text-xl">
             <span>Read More: </span>
